592b9bd92fb11cdf6f00b67b12fbe6b096797d3c
Florian Neveu <hello@fnev.eu>
2017-01-06 21:53:29 +0100

    Prepare installation and restore

---
+++ restore.sh
@@ -0,0 +1,20 @@
+#!env bash
+
+for file in $(find ~ -maxdepth 1 -name .zshrc -o -name .gitconfig); do
+    filename=$(basename $file)
+    latestBackup=$(cd ~; ls $filename"_"* 2>/dev/null | sort -rn | head -n 1)
+    homeDirectory=~
+    # Is $file a symlink?
+    if [ -L $file ]; then
+    echo "$filename is a symlink"
+    echo -n "Looking for backup..."
+    if [ "$latestBackup" != "" ]; then
+        echo " '$latestBackup'"
+        rm $file
+        mv $homeDirectory"/"$latestBackup $homeDirectory"/"$filename
+    else
+        echo " no backup found!"
+        rm $file
+    fi
+    fi
+done
