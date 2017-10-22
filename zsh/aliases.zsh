406480901e137ddc0dbf38b89f86fd1088b51103
Florian Neveu <hello@fnev.eu>
2017-10-22 12:08:02 +0200

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,6 +8,6 @@ alias nginx.stop='brew services stop nginx'
 alias nginx.restart='nginx.stop && nginx.start'
 
 ### PHP-fpm
-alias php-fpm.start="launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.php56.plist"
-alias php-fpm.stop="launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.php56.plist"
+alias php-fpm.start="brew services start homebrew/php/php72"
+alias php-fpm.stop="brew services stop homebrew/php/php72"
 alias php-fpm.restart='php-fpm.stop && php-fpm.start'
