83a23ba4a064977c9d74f28e86cce2da26af0102
Florian Neveu <hello@fnev.eu>
2019-08-05 22:04:57 +0200

    some tests

--- getpower.sh
+++ getpower.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
-
 if which tput >/dev/null 2>&1; then
     ncolors=$(tput colors)
 fi
@@ -21,6 +19,20 @@ else
     NORMAL=""
 fi
 
+printf "${BLUE}Installing Homebrew...${NORMAL}\n"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+printf "${BLUE}Installing zsh...${NORMAL}\n"
+brew install zsh zsh-completions
+# To activate these completions, add the following to your .zshrc:
+#   fpath=(/usr/local/share/zsh-completions $fpath)
+# You may also need to force rebuild `zcompdump`:
+#   rm -f ~/.zcompdump; compinit
+# Additionally, if you receive "zsh compinit: insecure directories" warnings when attempting
+# to load these completions, you may need to run this:
+#   chmod go-w '/usr/local/share'
+
+chsh -s /bin/zsh
+
 printf "${BLUE}Cloning Powerline fonts...${NORMAL}\n"
 env git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
@@ -36,11 +48,15 @@ env git clone --recursive https://github.com/fnev-eu/dotfiles.git ~/.dotfiles ||
     exit 1
 }
 
-echo "# shortcut to this dotfiles path is $ZSH
 export CZSH=~/.dotfiles
 
-for config_file ($CZSH/**/*.zsh) source $config_file
+echo "# shortcut to this dotfiles path is $ZSH
+export CZSH=~/.dotfiles
 " > ~/.zshrc
+
+for config_file in `find $CZSH -name "*.zsh" | grep -v '\.git'`; do
+    echo "source $config_file" >> ~/.zshrc
+done
 
 source .zshrc
 
