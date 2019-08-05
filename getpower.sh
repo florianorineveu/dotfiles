0ebb0ab2135d6e0715d53620d5afc6d00a1d9e67
Florian Neveu <hello@fnev.eu>
2019-08-06 00:20:10 +0200

    Silence!

--- getpower.sh
+++ getpower.sh
@@ -23,20 +23,20 @@ printf "${BLUE}Installing Homebrew...${NORMAL}\n\n"
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 printf "${BLUE}Installing zsh...${NORMAL}\n\n"
-brew install zsh zsh-completions
+brew install zsh zsh-completions >> /dev/null
 chmod go-w '/usr/local/share'
 chsh -s /bin/zsh
 
 printf "${BLUE}Cloning Powerline fonts...${NORMAL}\n\n"
-# env git clone https://github.com/powerline/fonts.git --depth=1
-# cd fonts
-# ./install.sh
-# cd ..
-# rm -rf fonts
+env git clone https://github.com/powerline/fonts.git --depth=1 --quiet
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
 printf "Utiliser la font Meslo LG M for Powerline - Regular 12\n\n"
 
 printf "${BLUE}Cloning dotfiles...${NORMAL}\n"
-env git clone --recursive https://github.com/fnev-eu/dotfiles.git ~/.dotfiles || {
+env git clone https://github.com/fnev-eu/dotfiles.git ~/.dotfiles --recursive --quiet || {
     printf "${YELLOW}Error:${NORMAL} git clone of dotfiles repo failed\n"
     exit 1
 }
@@ -60,25 +60,10 @@ setopt promptsubst
 " >> ~/.zshrc
 
 printf "${BLUE}Installing the fuck...${NORMAL}\n"
-brew install thefuck
+brew install thefuck >> /dev/null
 echo "
 # The Fuck configuration
 eval \$(thefuck --alias)
 " >> ~/.zshrc
 
 exec zsh -l
-
-# # Add zsh-completions
-# fpath=(/usr/local/share/zsh-completions $fpath)
-
-# # shortcut to this dotfiles path is 
-# export CZSH=~/.dotfiles
-
-# source /Users/0ri/.dotfiles/vagrant/aliases.zsh
-# source /Users/0ri/.dotfiles/zsh/aliases.zsh
-# source /Users/0ri/.dotfiles/git/aliases.zsh
-
-# source "/Volumes/BETA/Developpement/dotfiles/zsh/theme-agnoster/agnoster.zsh-theme"
-# setopt promptsubst
-
-# eval $(thefuck --alias)
