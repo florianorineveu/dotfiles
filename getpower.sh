27fc46adc05a4e3ae29b5f1d2cd412195ecc54be
Florian Neveu <hello@fnev.eu>
2020-05-18 02:18:08 +0200

    Initial commit for rework

--- getpower.sh
+++ getpower.sh
@@ -1,69 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if which tput >/dev/null 2>&1; then
-    ncolors=$(tput colors)
-fi
-if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-    RED="$(tput setaf 1)"
-    GREEN="$(tput setaf 2)"
-    YELLOW="$(tput setaf 3)"
-    BLUE="$(tput setaf 4)"
-    BOLD="$(tput bold)"
-    NORMAL="$(tput sgr0)"
-else
-    RED=""
-    GREEN=""
-    YELLOW=""
-    BLUE=""
-    BOLD=""
-    NORMAL=""
-fi
+DOTFILES_ROOT=~/.dotfiles
 
-printf "${BLUE}Installing Homebrew...${NORMAL}\n"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-printf "${BLUE}Installing zsh...${NORMAL}\n"
-brew install zsh zsh-completions >> /dev/null
-chmod go-w '/usr/local/share'
-chsh -s /bin/zsh
-
-printf "${BLUE}Cloning Powerline fonts...${NORMAL}\n"
-env git clone https://github.com/powerline/fonts.git --depth=1 --quiet
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
-printf "Utiliser la font Meslo LG M for Powerline - Regular 12\n"
-
-printf "${BLUE}Cloning dotfiles...${NORMAL}\n"
-env git clone https://github.com/fnev-eu/dotfiles.git ~/.dotfiles --recursive --quiet || {
-    printf "${YELLOW}Error:${NORMAL} git clone of dotfiles repo failed\n"
-    exit 1
+info () {
+    printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
-export CZSH=~/.dotfiles
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
 
-echo "# Add zsh-completions
-fpath=(/usr/local/share/zsh-completions \$fpath)
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
 
-# shortcut to this dotfiles path is $ZSH
-export CZSH=~/.dotfiles
-" > ~/.zshrc
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit 1
+}
 
-for config_file in `find $CZSH -name "*.zsh" | grep -v '\.git'`; do
-    echo "source \"$config_file\"" >> ~/.zshrc
-done
+install_dotfiles() {
+    info "Installing dotfiles in ${DOTFILES_ROOT} ..."
 
-echo "
-source \"$CZSH/zsh/theme-agnoster/agnoster.zsh-theme\"
-setopt promptsubst
-" >> ~/.zshrc
+    info "Check if dotfiles are already installed..."
+    if [[ -d "${DOTFILES_ROOT}" ]]
+    then
+        fail "${DOTFILES_ROOT} already exists on your filesystem."
+    fi
 
-printf "${BLUE}Installing the fuck...${NORMAL}\n"
-brew install thefuck >> /dev/null
-echo "
-# The Fuck configuration
-eval \$(thefuck --alias)
-" >> ~/.zshrc
+    info "Cloning repository..."
+    git clone https://github.com/fnev-eu/dofiles.git ~/.dotfiles --recursive --quiet || fail "Cloning repository failed."
+}
 
-exec zsh -l
+install_dotfiles
+
