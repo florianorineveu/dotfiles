9f4fde24a7db67cbfddc4bb4e49bc9a466e91fb0
Florian Neveu <hello@fnev.eu>
2020-05-20 17:29:45 +0200

    Fix order

--- getpower.sh
+++ getpower.sh
@@ -70,8 +70,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-install_brew
 install_dotfiles
+install_brew
 install_macos_settings
 
 printf ""
