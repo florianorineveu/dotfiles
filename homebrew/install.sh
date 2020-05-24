817ed94e35dd6577f8de979c85b8502c43901f05
Florian Neveu <hello@fnev.eu>
2020-05-24 18:52:00 +0200

    [homebrew] Fix packages installation

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,31 +1,44 @@
-if [ "$(uname -s)" == "Darwin" ]
+if [[ "$(uname -s)" == "Darwin" ]]
 then
-    if ! [ -x "$(command -v brew)" ];
+    if ! [[ -x "$(command -v brew)" ]];
     then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
 
-    if [ -f ./homebrew/brew ]
+    brew update >/dev/null
+
+    NEW_PACKAGE=false
+
+    if [[ -f ./homebrew/brew ]];
     then
         while IFS= read -r line || [[ -n "$line" ]]; do
-            if ! [[ brew ls --versions $line > /dev/null ]]
+            if [[ -z "$(brew ls --versions $line)" ]];
             then
                 printf "\r  [ \033[00;34m..\033[0m ] Installing %s...\n" "$line"
                 brew install $line >/dev/null
+                printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s intalled.\n" "$line"
+
+                NEW_PACKAGE=true
             fi
         done < ./homebrew/brew
     fi
 
-    if [ -f ./homebrew/cask ]
+    if [[ -f ./homebrew/cask ]]
     then
         while IFS= read -r line || [[ -n "$line" ]]; do
-            if ! [[ brew cask ls --versions $line > /dev/null ]]
+            if [[ -z "brew cask ls --versions $line" ]];
             then
                 printf "\r  [ \033[00;34m..\033[0m ] Installing %s...\n" "$line"
                 brew cask install $line >/dev/null
+                printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s intalled.\n" "$line"
+
+                NEW_PACKAGE=true
             fi
         done < ./homebrew/cask
     fi
 
-    brew cleanup
+    if [[ "${NEW_PACKAGE}" = true ]];
+    then
+        brew cleanup >/dev/null
+    fi
 fi
