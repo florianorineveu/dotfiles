3352519b98c1c0b1b02142bd65f4184d6e5e707e
Florian Neveu <hello@fnev.eu>
2020-05-24 17:39:46 +0200

    [homebrew] Speed up installer

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,16 +8,22 @@ then
     if [ -f ./homebrew/brew ]
     then
         while IFS= read -r line || [[ -n "$line" ]]; do
-            printf "\r  [ \033[00;34m..\033[0m ] Installing %s...\n" "$line"
-            brew install $line >/dev/null
+            if ! [brew ls --versions $line > /dev/null; ]
+            then
+                printf "\r  [ \033[00;34m..\033[0m ] Installing %s...\n" "$line"
+                brew install $line >/dev/null
+            fi
         done < ./homebrew/brew
     fi
 
     if [ -f ./homebrew/cask ]
     then
         while IFS= read -r line || [[ -n "$line" ]]; do
-            printf "\r  [ \033[00;34m..\033[0m ] Installing %s...\n" "$line"
-            brew cask install $line >/dev/null
+            if ! [brew cask ls --versions $line > /dev/null; ]
+            then
+                printf "\r  [ \033[00;34m..\033[0m ] Installing %s...\n" "$line"
+                brew cask install $line >/dev/null
+            fi
         done < ./homebrew/cask
     fi
 
