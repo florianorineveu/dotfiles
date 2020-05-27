3fab23e005d2cebda1e6e857a2df4b5abe4d0ac5
Florian Neveu <hello@fnev.eu>
2020-05-27 15:40:32 +0200

    [zsh] Some tests

---
+++ zsh/config.zsh
@@ -0,0 +1,15 @@
+autoload -U $ZSH/functions/*(:t)
+
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+# Pretty history
+# Thanks to github.com/w0ng
+# https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
