3ea650ce665d2930c9d03812ed1b9b5a128b9381
Florian Neveu <hello@fnev.eu>
2020-05-27 16:50:02 +0200

    [git] Add grh alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -25,3 +25,5 @@ alias gfp="git fetch --all --prune --verbose"
 
 alias gs="git status -sb"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+grh="git reset --hard HEAD"
