3693589af1831317901c5fde6d2a5170205e5105
Florian Neveu <hello@fnev.eu>
2020-05-27 18:00:58 +0200

    [installer] decoration and clean

--- getpower.sh
+++ getpower.sh
@@ -1,26 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 CURRENT_LOCATION=$(pwd -P)
 DOTFILES_ROOT=~/.dotfiles
 
-info () {
-    printf "\r  [ \033[00;34m..\033[0m ] %s\n" "$1"
-}
-
-user () {
-  printf "\r  [ \033[0;33m??\033[0m ] %s\n" "$1"
-}
-
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
-}
-
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
-  echo ''
-  exit 1
-}
-
+# FUNCTIONS
+#####################################
 install_dotfiles() {
     info "Check if dotfiles are already installed..."
     if [[ -d "${DOTFILES_ROOT}" ]]
@@ -33,7 +17,7 @@ install_dotfiles() {
         git clone https://github.com/fnev-eu/dotfiles.git ${DOTFILES_ROOT} --recursive --quiet || fail "Cloning repository failed."
     fi
 
-    success "dotfiles à jour."
+    success "dotfiles are now up-to-date."
 
     for src in $(find -H "${DOTFILES_ROOT}" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
     do
@@ -49,7 +33,7 @@ install_brew() {
 
     if [[ $? -eq 0 ]]
     then
-        success "Brew installed"
+        success "Brew and packages installed"
     else
         fail "Brew failed"
     fi
@@ -71,7 +55,39 @@ install_macos_settings() {
     fi
 }
 
-info "Please type your password in order to setup homebrew and some configurations for MacOS..."
+# SCRIPT TOOLS
+#####################################
+info () {
+    printf "\r  [ \033[00;34m..\033[0m ] %s\n" "$1"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] %s\n" "$1"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+  echo ''
+  exit 1
+}
+
+# MAIN
+#####################################
+cat <<EOL
+      _       _
+     | |     | |
+   __| | ___ | |_
+  / _\` |/ _ \\| __|
+ | (_| | (_) | |_
+  \\__,_|\\___/ \\__|
+
+EOL
+
+info "Please type your password in order to setup homebrew and some configurations for your OS..."
 
 # Ask for the administrator password upfront
 sudo -v
