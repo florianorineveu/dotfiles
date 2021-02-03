1aa93a58ab3d6266f14a6b46c78941c8f30e9583
Florian Neveu <hello@fnev.eu>
2021-02-03 22:11:21 +0100

    comment macos settings

--- macos/settings.sh
+++ macos/settings.sh
@@ -1,187 +1,187 @@
-# Sets some defaults settings for MacOS
+## Sets some defaults settings for MacOS
+##
+## The idea and some settings come from
+## https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 #
-# The idea and some settings come from
-# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
-
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we’re about to change
-#osascript -e 'tell application "System Preferences" to quit'
-
-# Set computer name (as done via System Preferences → Sharing)
-#sudo scutil --set ComputerName GLaDOS
-#sudo scutil --set HostName GLaDOS
-#sudo scutil --set LocalHostName GLaDOS
-
-# Disable the sound effects on boot
-#sudo nvram SystemAudioVolume=" "
-
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `Nlsv`, `clmv`, `glyv`
-#defaults write com.apple.Finder FXPreferredViewStyle Nlsv
-
-# Expand the following File Info panes:
-# “General”, “Open with”, and “Sharing & Permissions”
-#defaults write com.apple.finder FXInfoPanesExpanded -dict \
-#    General -bool true \
-#    OpenWith -bool true \
-#    Privileges -bool true
-
-# Save to disk (not to iCloud) by default
-#defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-# Disable the “Are you sure you want to open this application?” dialog
-#defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-# Reveal IP address, hostname, OS version, etc. when clicking the clock
-# in the login window
-#sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
-
-# Disable smart quotes as they’re annoying when typing code
-#defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-
-# Disable auto-correct
-#defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-
-# Show the ~/Library folder.
-#chflags nohidden ~/Library
-#[[ $(xattr ~/Library) = *com.apple.FinderInfo* ]] && xattr -d com.apple.FinderInfo ~/Library
-
-# Set a really fast key repeat.
-#defaults write NSGlobalDomain KeyRepeat -int 1
-
-# Show icons for hard drives, servers, and removable media on the desktop
-#defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-#defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-#defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-#defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-# Run the screensaver if we're in the bottom-left hot corner.
-#defaults write com.apple.dock wvous-br-corner -int 5
-#defaults write com.apple.dock wvous-br-modifier -int 0
-
-# Never go into computer sleep mode
-#sudo systemsetup -setcomputersleep Off > /dev/null
-
-# Save screenshots to the desktop
-#defaults write com.apple.screencapture location -string "${HOME}/Desktop"
-
-# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-#defaults write com.apple.screencapture type -string "png"
-
-# Set Desktop as the default location for new Finder windows
-# For desktop, use `PfDe` and `file://${HOME}/Desktop/`
-# For other paths, use `PfLo` and `file:///full/path/here/`
-#defaults write com.apple.finder NewWindowTarget -string "PfHm"
-#defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
-
-# Finder: show status bar
-#defaults write com.apple.finder ShowStatusBar -bool true
-
-# Finder: show path bar
-#defaults write com.apple.finder ShowPathbar -bool true
-
-# Display full POSIX path as Finder window title
-#defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-
-# Keep folders on top when sorting by name
-#defaults write com.apple.finder _FXSortFoldersFirst -bool true
-
-# When performing a search, search the current folder by default
-#defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-
-# Disable the warning when changing a file extension
-#defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-# Automatically open a new Finder window when a volume is mounted
-#defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-#defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-#defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
-
-# Show the /Volumes folder
-#sudo chflags nohidden /Volumes
-
-# Disable screenshot shadows on a Mac
-#defaults write com.apple.screencapture disable-shadow -bool true;
-# Revert : defaults write com.apple.screencapture disable-shadow -bool false; killall SystemUIServer
-
-# Display the file extensions in Finder
-#defaults write NSGlobalDomain AppleShowAllExtensions -bool true;
-# Revert : defaults write NSGlobalDomain AppleShowAllExtensions -bool false; killall Finder
-
-# Display additional information on the loginscreen
-#sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
-# Revert : sudo defaults delete /Library/Preferences/com.apple.loginwindow AdminHostInfo
-
-# Use plain text as default format in TextEdit
-#defaults write com.apple.TextEdit RichText -int 0
-# Revert : defaults delete com.apple.TextEdit RichText
-
-# Stop Photos from opening automatically on your Mac
-#defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
-# Revert : defaults -currentHost delete com.apple.ImageCapture disableHotPlug
-
-# Adding `Quit` option to Finder on a Mac
-#defaults write com.apple.finder QuitMenuItem -bool true;
-# Revert : defaults write com.apple.finder QuitMenuItem -bool false; killall Finder
-
-# Enable the expand save panel by default
-#defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-#defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-# Revert : defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool false
-# defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool false
-
-# Disable automatically rearrange Spaces based on recent use
-#defaults write com.apple.dock mru-spaces -bool false
-# Revert : defaults write com.apple.dock mru-spaces -bool true
-
-# Make Crash Reporter appear as a notification
-defaults write com.apple.CrashReporter UseUNC 1
-# Revert : defaults write com.apple.CrashReporter UseUNC 0
-
-###############################################################################
-# Safari & WebKit                                                             #
-###############################################################################
-
-# Privacy: don’t send search queries to Apple
-defaults write com.apple.Safari UniversalSearchEnabled -bool false
-defaults write com.apple.Safari SuppressSearchSuggestions -bool true
-
-# Show the full URL in the address bar (note: this still hides the scheme)
-defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
-
-# Set Safari’s home page to `about:blank` for faster loading
-defaults write com.apple.Safari HomePage -string "about:blank"
-
-# Prevent Safari from opening ‘safe’ files automatically after downloading
-defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
-
-# Set up Safari for development.
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-# Enable continuous spellchecking
-defaults write com.apple.Safari WebContinuousSpellCheckingEnabled -bool true
-# Disable auto-correct
-defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
-
-# Disable AutoFill
-defaults write com.apple.Safari AutoFillFromAddressBook -bool false
-defaults write com.apple.Safari AutoFillPasswords -bool false
-defaults write com.apple.Safari AutoFillCreditCardData -bool false
-defaults write com.apple.Safari AutoFillMiscellaneousForms -bool false
-
-# Enable “Do Not Track”
-defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
-
-# Update extensions automatically
-defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
-
-killall Finder
-killall SystemUIServer
-
-# Require password immediately after sleep or screen saver begins
-/usr/bin/profiles -I -F ./macos/askforpassworddelay.mobileconfig
+## Close any open System Preferences panes, to prevent them from overriding
+## settings we’re about to change
+##osascript -e 'tell application "System Preferences" to quit'
+#
+## Set computer name (as done via System Preferences → Sharing)
+##sudo scutil --set ComputerName GLaDOS
+##sudo scutil --set HostName GLaDOS
+##sudo scutil --set LocalHostName GLaDOS
+#
+## Disable the sound effects on boot
+##sudo nvram SystemAudioVolume=" "
+#
+## Use list view in all Finder windows by default
+## Four-letter codes for the other view modes: `icnv`, `Nlsv`, `clmv`, `glyv`
+##defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+#
+## Expand the following File Info panes:
+## “General”, “Open with”, and “Sharing & Permissions”
+##defaults write com.apple.finder FXInfoPanesExpanded -dict \
+##    General -bool true \
+##    OpenWith -bool true \
+##    Privileges -bool true
+#
+## Save to disk (not to iCloud) by default
+##defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+#
+## Disable the “Are you sure you want to open this application?” dialog
+##defaults write com.apple.LaunchServices LSQuarantine -bool false
+#
+## Reveal IP address, hostname, OS version, etc. when clicking the clock
+## in the login window
+##sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+#
+## Disable smart quotes as they’re annoying when typing code
+##defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+#
+## Disable auto-correct
+##defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+#
+## Show the ~/Library folder.
+##chflags nohidden ~/Library
+##[[ $(xattr ~/Library) = *com.apple.FinderInfo* ]] && xattr -d com.apple.FinderInfo ~/Library
+#
+## Set a really fast key repeat.
+##defaults write NSGlobalDomain KeyRepeat -int 1
+#
+## Show icons for hard drives, servers, and removable media on the desktop
+##defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+##defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+##defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+##defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+#
+## Run the screensaver if we're in the bottom-left hot corner.
+##defaults write com.apple.dock wvous-br-corner -int 5
+##defaults write com.apple.dock wvous-br-modifier -int 0
+#
+## Never go into computer sleep mode
+##sudo systemsetup -setcomputersleep Off > /dev/null
+#
+## Save screenshots to the desktop
+##defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+#
+## Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+##defaults write com.apple.screencapture type -string "png"
+#
+## Set Desktop as the default location for new Finder windows
+## For desktop, use `PfDe` and `file://${HOME}/Desktop/`
+## For other paths, use `PfLo` and `file:///full/path/here/`
+##defaults write com.apple.finder NewWindowTarget -string "PfHm"
+##defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+#
+## Finder: show status bar
+##defaults write com.apple.finder ShowStatusBar -bool true
+#
+## Finder: show path bar
+##defaults write com.apple.finder ShowPathbar -bool true
+#
+## Display full POSIX path as Finder window title
+##defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+#
+## Keep folders on top when sorting by name
+##defaults write com.apple.finder _FXSortFoldersFirst -bool true
+#
+## When performing a search, search the current folder by default
+##defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+#
+## Disable the warning when changing a file extension
+##defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+#
+## Automatically open a new Finder window when a volume is mounted
+##defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+##defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+##defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+#
+## Show the /Volumes folder
+##sudo chflags nohidden /Volumes
+#
+## Disable screenshot shadows on a Mac
+##defaults write com.apple.screencapture disable-shadow -bool true;
+## Revert : defaults write com.apple.screencapture disable-shadow -bool false; killall SystemUIServer
+#
+## Display the file extensions in Finder
+##defaults write NSGlobalDomain AppleShowAllExtensions -bool true;
+## Revert : defaults write NSGlobalDomain AppleShowAllExtensions -bool false; killall Finder
+#
+## Display additional information on the loginscreen
+##sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+## Revert : sudo defaults delete /Library/Preferences/com.apple.loginwindow AdminHostInfo
+#
+## Use plain text as default format in TextEdit
+##defaults write com.apple.TextEdit RichText -int 0
+## Revert : defaults delete com.apple.TextEdit RichText
+#
+## Stop Photos from opening automatically on your Mac
+##defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+## Revert : defaults -currentHost delete com.apple.ImageCapture disableHotPlug
+#
+## Adding `Quit` option to Finder on a Mac
+##defaults write com.apple.finder QuitMenuItem -bool true;
+## Revert : defaults write com.apple.finder QuitMenuItem -bool false; killall Finder
+#
+## Enable the expand save panel by default
+##defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+##defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+## Revert : defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool false
+## defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool false
+#
+## Disable automatically rearrange Spaces based on recent use
+##defaults write com.apple.dock mru-spaces -bool false
+## Revert : defaults write com.apple.dock mru-spaces -bool true
+#
+## Make Crash Reporter appear as a notification
+#defaults write com.apple.CrashReporter UseUNC 1
+## Revert : defaults write com.apple.CrashReporter UseUNC 0
+#
+################################################################################
+## Safari & WebKit                                                             #
+################################################################################
+#
+## Privacy: don’t send search queries to Apple
+#defaults write com.apple.Safari UniversalSearchEnabled -bool false
+#defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+#
+## Show the full URL in the address bar (note: this still hides the scheme)
+#defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+#
+## Set Safari’s home page to `about:blank` for faster loading
+#defaults write com.apple.Safari HomePage -string "about:blank"
+#
+## Prevent Safari from opening ‘safe’ files automatically after downloading
+#defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+#
+## Set up Safari for development.
+#defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+#defaults write com.apple.Safari IncludeDevelopMenu -bool true
+#defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+#defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+#defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true
+#defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+#
+## Enable continuous spellchecking
+#defaults write com.apple.Safari WebContinuousSpellCheckingEnabled -bool true
+## Disable auto-correct
+#defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
+#
+## Disable AutoFill
+#defaults write com.apple.Safari AutoFillFromAddressBook -bool false
+#defaults write com.apple.Safari AutoFillPasswords -bool false
+#defaults write com.apple.Safari AutoFillCreditCardData -bool false
+#defaults write com.apple.Safari AutoFillMiscellaneousForms -bool false
+#
+## Enable “Do Not Track”
+#defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
+#
+## Update extensions automatically
+#defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
+#
+#killall Finder
+#killall SystemUIServer
+#
+## Require password immediately after sleep or screen saver begins
+#/usr/bin/profiles -I -F ./macos/askforpassworddelay.mobileconfig
